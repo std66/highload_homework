0ff5f33eb9f1e3d72456ebb0f724f22448adf182
Sinku Tamás <sinkutamas@gmail.com>
2024-09-26 22:00:17 +0200

    Move action methods from Program.cs

---
+++ src/TomiSoft.HighLoad.App/ActionMethods.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TomiSoft.HighLoad.App.Models.Api;
+
+namespace TomiSoft.HighLoad.App;
+
+public static class ActionMethods {
+    public static string Healthz() => "healthy";
+
+    public static IResult RegisterVehicle() {
+        return Results.Created();
+    }
+
+    public static string GetCountOfRegisteredVehicles() => "0";
+
+    public static RegisteredVehicleDto GetVehicleById(string uuid) {
+        return GetTestData();
+    }
+
+    public static SearchVehicleResultDto SearchVehicle([FromQuery, Required] string q) {
+        return new SearchVehicleResultDto() {
+            GetTestData()
+        };
+    }
+
+    private static RegisteredVehicleDto GetTestData() {
+        return new RegisteredVehicleDto() {
+            Uuid = Guid.NewGuid(),
+            ForgalmiErvenyes = new DateOnly(2025, 02, 01),
+            Rendszam = "ABC123",
+            Tulajdonos = "Kiss István",
+            Adatok = ["teszt1", "teszt2"]
+        };
+    }
+
+    public static void RegisterEndpoints(this WebApplication app) {
+        app.MapGet("/healthz", ActionMethods.Healthz);
+
+        app.MapPost("/jarmuvek", ActionMethods.RegisterVehicle);
+        app.MapGet("/jarmuvek", ActionMethods.GetCountOfRegisteredVehicles);
+        app.MapGet("/jarmuvek/{uuid}", ActionMethods.GetVehicleById);
+        app.MapGet("/kereses", ActionMethods.SearchVehicle);
+    }
+}
--- src/TomiSoft.HighLoad.App/Program.cs
+++ src/TomiSoft.HighLoad.App/Program.cs
@@ -1,17 +1,15 @@
-using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Text.Json;
+using TomiSoft.HighLoad.App;
 using TomiSoft.HighLoad.App.Models.Api;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options => {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
-
 builder.Services
-    .AddLogging();
+    .AddLogging()
+    .ConfigureHttpJsonOptions(options => {
+        options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+    });
 
 var app = builder.Build();
 
@@ -31,32 +29,6 @@ app.Use(async (context, next) => {
     }
 });
 
-app.MapGet("/healthz", () => "healthy");
-
-app.MapPost("/jarmuvek", () => {
-    return Results.Created();
-});
-
-app.MapGet("/jarmuvek", () => "0");
-
-app.MapGet("/jarmuvek/{uuid}", (string uuid) => {
-    return GetTestData();
-});
-
-app.MapGet("/kereses", ([FromQuery, Required] string q) => {
-    return new SearchVehicleResultDto() {
-        GetTestData()
-    };
-});
+app.RegisterEndpoints();
 
 app.Run();
-
-RegisteredVehicleDto GetTestData() {
-    return new RegisteredVehicleDto() {
-        Uuid = Guid.NewGuid(),
-        ForgalmiErvenyes = new DateOnly(2025, 02, 01),
-        Rendszam = "ABC123",
-        Tulajdonos = "Kiss István",
-        Adatok = ["teszt1", "teszt2"]
-    };
-}
