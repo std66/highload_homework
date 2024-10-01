018246fd83895e408a794d02bb3042cca8ec7810
Sinku Tamás <sinkutamas@gmail.com>
2024-10-01 22:42:13 +0200

    Add docker build, compose, load balancing

---
+++ local_run/create_docker_image.bat
@@ -0,0 +1,3 @@
+cd ..
+docker build -t std66/highload_homework:latest . --progress=plain
+pause
---
+++ local_run/db-init.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION pg_trgm;
+
+CREATE TABLE jarmu (
+    uuid UUID PRIMARY KEY,
+    rendszam VARCHAR(20) NOT NULL UNIQUE,
+    tulajdonos VARCHAR(200) NOT NULL,
+    forgalmi_ervenyes DATE NOT NULL,
+    eredeti_adatok JSON
+);
+
+CREATE TABLE adatok (
+    uuid UUID REFERENCES jarmu(uuid) ON DELETE CASCADE,
+    adat VARCHAR(200) NOT NULL
+);
+
+CREATE INDEX idx_adat_gin ON adatok USING gin (adat gin_trgm_ops);
